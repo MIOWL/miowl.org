3ef153a8a71d1eb414ee20c33fca917bbf46a06c
Alan Wynn <wynn_45@hotmail.com>
2012-04-24 08:52:40 +0100

    create some basic pages

---
+++ application/views/auth/login.php
@@ -0,0 +1,9 @@
+<?php $this->load->view('template/header'); ?>
+
+	<h1>Login Page!</h1>
+
+	<div id="body">
+		<p>The page you are looking at is under construction.</p>
+	</div>
+
+<?php $this->load->view('template/footer'); ?>
