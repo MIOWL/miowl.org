c3e249b6bbcb8f4089f73647df1c40c6503cc6f6
Alan Wynn <wynn_45@hotmail.com>
2012-04-24 09:13:07 +0100

    add javascript

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,5 +1,36 @@
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
 
+
+<!-- ---------------------------------------------------------------------------------------------- -->
+<!-- -------------------------------------- JAVASCRRIPT HERE -------------------------------------- -->
+<!-- ---------------------------------------------------------------------------------------------- -->
+
+<!-- jQuery -->
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
+<script>
+	window.jQuery || document.write('<script src="/js/jquery.min.js"><\/script>\n<script src="/js/jquery-ui.min.js"><\/script>')
+</script>
+<script type="text/javascript" src="/js/uni-form.jquery.min.js"></script>
+<script type="text/javascript" src="/js/tips.js"></script>
+
+
+<!-- Google Analytics -->
+<script type="text/javascript">/*
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-29923472-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+*/</script>
+
+<!-- ---------------------------------------------------------------------------------------------- -->
+
+
 </body>
 </html>
