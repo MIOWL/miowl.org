9cdb30ccb36610f50603cb1d609eade9993c76a5
Alan Wynn <wynn_45@hotmail.com>
2012-04-25 09:26:06 +0100

    nav update

--- application/views/template/nav.php
+++ application/views/template/nav.php
@@ -4,13 +4,19 @@
 		if ($this->session->userdata('authed'))
 		{
 			# Logged in NAV
-			print "<a href='user/logout' title='Logout'>logout</a>";
+			print "<a href='user/logout' title='Logout'>logout</a>";			# logout
+			print " | ";														# spacer
+			print "<a href='upload' title='Upload a new file'>upload</a>";		# upload
 		}
 		else
 		{
 			# Default NAV
-			print "<a href='user/login' title='Login'>login</a>";
+			print "<a href='user/login' title='Login'>login</a>"; 				# login
 		}
+
+		# These are non specific nav options
+		print " | ";															# spacer
+		print "<a href='search' title='Search for a file'>search</a>";			# search
 
 		?>
 	</div>
