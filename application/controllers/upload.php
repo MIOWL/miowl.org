c74c7012fe4d8f92b0428ac1d6eafe3c81a8b44d
Alan Wynn <wynn_45@hotmail.com>
2012-04-25 23:50:16 +0100

    add file_types for view

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -50,10 +50,69 @@ class Upload extends CI_Controller {
         if (!$this->login_check('upload'))
             return;
 
+        // What are the allowed file types? [seperate via pipe (|)]
+        $file_types = 'txt|pdf|doc|docx|rtf';
+
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = $file_types;
+        $config['max_size'] = '10240'; // 10MB
+        $config['encrypt_name'] = TRUE;
+
         $page_data = array();
-        $page_data['page_title'] = 'Upload';
         $page_data['error'] = NULL;
-        $this->load->view('pages/upload_form', $page_data);
+        $page_data['file_types'] = $file_types;
+
+        $this->load->library('upload', $config);
+
+        if($this->form_validation->run())
+        {
+            if (!$this->upload->do_upload())
+            {
+                $page_data['page_title'] = 'Upload Failure';
+                $page_data['error'] = $this->upload->display_errors();
+
+                $this->load->view('pages/upload_form', $page_data);
+            }
+            else
+            {
+                $upload_data = $this->upload->data();
+
+                $upload_user        = $this->session->userdata('username');
+                $owl                = $this->session->userdata('owl');
+                $file_name          = $upload_data['file_name'];
+                $full_path          = $upload_data['full_path'];
+                $upload_catagory    = 'n/a';
+                $file_type          = $upload_data['file_type'];
+                $client_name        = $upload_data['client_name'];
+                $file_size          = $upload_data['file_size'];
+                $file_ext           = $upload_data['file_ext'];
+                $description        = 'n/a';
+
+                $this->upload_model->add_upload(
+                            $upload_user,
+                            $owl,
+                            $file_name,
+                            $full_path,
+                            $upload_catagory,
+                            $file_type,
+                            $client_name,
+                            $file_size,
+                            $file_ext,
+                            $description
+                          );
+
+                $page_data['page_title'] = 'Upload Success';
+                $page_data['upload_data'] = $upload_data;
+
+                $this->load->view('pages/upload_success', $page_data);
+            }
+        }
+        else
+        {
+            $page_data = array();
+            $page_data['page_title'] = 'Upload';
+            $this->load->view('pages/upload_form', $page_data);
+        }
     }
     //------------------------------------------------------------------
 
