6233da13429fb73a7e76052826342de41d0214ab
Alan Wynn <wynn_45@hotmail.com>
2012-04-25 19:45:06 +0100

    nav fix

--- application/views/template/nav.php
+++ application/views/template/nav.php
@@ -4,21 +4,23 @@
 		if ($this->session->userdata('authed'))
 		{
 			# Logged in NAV
-			print "<a href='user/logout' title='Logout'>logout</a>";			# logout
-			print " | ";														# spacer
-			print "<a href='upload' title='Upload a new file'>upload</a>";		# upload
+			print "<a href='". site_url('user/logout') . "' title='Logout'>logout</a>";				# logout
+			print " | ";																			# spacer
+			print "<a href='". site_url('upload') . "' title='Upload a new file'>upload</a>";		# upload
 		}
 		else
 		{
 			# Default NAV
-			print "<a href='user/login' title='Login'>login</a>"; 				# login
+			print "<a href='". site_url('user/login') . "' title='Login'>login</a>"; 				# login
 		}
 
 		# These are non specific nav options
-		print " | ";															# spacer
-		print "<a href='search' title='Search for a file'>search</a>";			# search
-		print " | ";															# spacer
-		print "<a href='about' title='About the site'>about</a>";				# about
+		print " | ";																				# spacer
+		print "<a href='". site_url('browse') . "' title='Browse all the files'>browse</a>";		# browse
+		print " | ";																				# spacer
+		print "<a href='". site_url('search') . "' title='Search for a file'>search</a>";			# search
+		print " | ";																				# spacer
+		print "<a href='". site_url('about') . "' title='About the site'>about</a>";				# about
 
 		?>
 	</div>
