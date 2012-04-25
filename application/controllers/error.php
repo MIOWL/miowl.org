dec7ef7c4c4b25e82c6a6a7ff592617bcee164e2
Alan Wynn <wynn_45@hotmail.com>
2012-04-25 10:21:53 +0100

    custom 404 page fix

--- application/controllers/error.php
+++ application/controllers/error.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Error extends CI_Controller {
 
     //=================================================================================
     // :private vars
