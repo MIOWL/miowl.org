9c015b26363fcfe2bade3d43de9e89ab0080aae2
Alan Wynn <wynn_45@hotmail.com>
2012-04-26 18:07:33 +0100

    code cleanup after debug

--- application/views/pages/upload_success.php
+++ application/views/pages/upload_success.php
@@ -11,39 +11,15 @@
 		<ul>
             <li>upload_user: <?php print $this->session->userdata('username'); ?></li>
             <li>owl: <?php print $this->session->userdata('owl'); ?></li>
-            <li>file_name: <?php print is_null($this->input->post('filename')) ? $upload_data['file_name'] : $this->input->post('filename'); ?></li>
+            <li>file_name: <?php print $this->input->post('filename') == "" ? $upload_data['client_name'] : $this->input->post('filename'); ?></li>
             <li>full_path: <?php print $upload_data['full_path']; ?></li>
             <li>upload_catagory: <?php print $this->input->post('catagory'); ?></li>
             <li>file_type: <?php print $upload_data['file_type']; ?></li>
             <li>client_name: <?php print $upload_data['client_name']; ?></li>
             <li>file_size: <?php print $upload_data['file_size']; ?></li>
             <li>file_ext: <?php print $upload_data['file_ext']; ?></li>
-            <li>description: <?php print str_replace(array("\r\n","\r","\n"), '\n', trim($this->input->post('description'))); ?></li>
+            <li>description: <?php print trim($this->input->post('description')); ?></li>
         </ul>
-
-        <br>
-        <hr>
-        <br>
-
-		<ul>
-			<?php foreach ($upload_data as $item => $value):?>
-				<li><?php echo $item;?>: <?php echo $value;?></li>
-			<?php endforeach; ?>
-		</ul>
-
-        <br>
-        <hr>
-        <br>
-
-		<ul>
-			<?php foreach ($this->input->post(NULL, TRUE) as $item => $value):?>
-				<li><?php echo $item;?>: "<?php echo $value;?>"</li>
-			<?php endforeach; ?>
-		</ul>
-
-		<p>
-			<?php echo anchor('upload', 'Upload Another File!'); ?>
-		</p>
 
 	</div>
 
