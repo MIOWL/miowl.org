b6faae57aa3d0bf8d3cb00ddc1d67ecc4ce2f709
Alan Wynn <wynn_45@hotmail.com>
2012-04-26 16:43:03 +0100

    upload success view update

--- application/views/pages/upload_success.php
+++ application/views/pages/upload_success.php
@@ -9,16 +9,16 @@
 	<div id="body">
 
 		<ul>
-            <li>upload_user: <?php print $this->session->userdata('username'); ?><li>
-            <li>owl: <?php print $this->session->userdata('owl'); ?><li>
-            <li>file_name: <?php print is_null($this->input->post('filename')) ? $upload_data['file_name'] : $this->input->post('filename'); ?><li>
-            <li>full_path: <?php print $upload_data['full_path']; ?><li>
-            <li>upload_catagory: <?php print $this->input->post('catagory'); ?><li>
-            <li>file_type: <?php print $upload_data['file_type']; ?><li>
-            <li>client_name: <?php print $upload_data['client_name']; ?><li>
-            <li>file_size: <?php print $upload_data['file_size']; ?><li>
-            <li>file_ext: <?php print $upload_data['file_ext']; ?><li>
-            <li>description: <?php print htmlspecialchars(trim($this->input->post('description'))); ?><li>
+            <li>upload_user: <?php print $this->session->userdata('username'); ?></li>
+            <li>owl: <?php print $this->session->userdata('owl'); ?></li>
+            <li>file_name: <?php print is_null($this->input->post('filename')) ? $upload_data['file_name'] : $this->input->post('filename'); ?></li>
+            <li>full_path: <?php print $upload_data['full_path']; ?></li>
+            <li>upload_catagory: <?php print $this->input->post('catagory'); ?></li>
+            <li>file_type: <?php print $upload_data['file_type']; ?></li>
+            <li>client_name: <?php print $upload_data['client_name']; ?></li>
+            <li>file_size: <?php print $upload_data['file_size']; ?></li>
+            <li>file_ext: <?php print $upload_data['file_ext']; ?></li>
+            <li>description: <?php print htmlspecialchars(trim($this->input->post('description'))); ?></li>
         </ul>
 
         <br>
