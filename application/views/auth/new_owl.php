1ec73e1fd36f1fc9530488e734567fa1805812f7
Alan Wynn <wynn_45@hotmail.com>
2012-04-28 01:01:37 +0100

    owl choice view error fix

--- application/views/auth/new_owl.php
+++ application/views/auth/new_owl.php
@@ -83,7 +83,7 @@
                     <label for="province">Province</label>
                     <select name="province" id="province" class="textInput medium" autocompelete="OFF" />
                         <option value="select" <?php echo set_select('province', 'select', TRUE); ?>>Select</option>
-                        <?php foreach ($province) : ?>
+                        <?php foreach $province : ?>
                             <option value="<?php print $province; ?>" <?php echo set_select('province', $province); ?>><?php print $province; ?></option>
                         <?php endforeach; ?>
                     </select>
