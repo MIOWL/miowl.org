64a3363a080ade94b20c8289adc3f15a3ddc9bc5
Alan Wynn <wynn_45@hotmail.com>
2012-05-03 20:42:30 +0100

    remember to rename the class

--- application/libraries/owlmail.php
+++ application/libraries/owlmail.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Usermail {
+class Owlmail {
 
 
     //=================================================================================
