a053c04c63629ad93198bf0563305d8cbb2d61bc
Alan Wynn <wynn_45@hotmail.com>
2012-05-03 22:10:41 +0100

    debug

--- application/models/owl_model.php
+++ application/models/owl_model.php
@@ -127,6 +127,8 @@ class Owl_model extends CI_Model {
 
         $this->db->insert('owls', $insert_data);
 
+        die($this->db->insert_id());
+
         return $this->db->insert_id();
     }
     //------------------------------------------------------------------
