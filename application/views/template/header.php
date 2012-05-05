1aff0698c80fb69db165671eff6eb6194f7c4506
Alan Wynn <wynn_45@hotmail.com>
2012-05-05 20:59:36 +0100

    js in head

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -15,9 +15,6 @@
 <!-- jQuery -->
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
-<script>
-	window.jQuery || document.write('<script src="<?php print site_url('/js/jquery.min.js'); ?>"><\/script>\n<script src="<?php print site_url('/js/jquery-ui.min.js'); ?>"><\/script>')
-</script>
 <script type="text/javascript" src="<?php print site_url('/js/miowl.js'); ?>"></script>
 <script type="text/javascript" src="<?php print site_url('/js/uni-form.jquery.min.js'); ?>"></script>
 <script type="text/javascript" src="<?php print site_url('/js/tips.js'); ?>"></script>
