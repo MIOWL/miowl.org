dc07c1df9b768e81801d2c0e724750ee40e04e1b
Alan Wynn <wynn_45@hotmail.com>
2012-05-06 18:16:41 +0100

    side nav updates

--- application/views/pages/owl_nav.php
+++ application/views/pages/owl_nav.php
@@ -1,22 +1,22 @@
 	<ul>
 		<li style="list-style-type: none">members</li>
 		<ul>
-			<li class="owl_nav_button"><a href="<?php print site_url('owl/members'); ?>">list</a></li>
-			<li class="owl_nav_button"><a href="<?php print site_url('owl/members/requests'); ?>">requests</a></li>
-			<li class="owl_nav_button"><a href="<?php print site_url('owl/members/invite'); ?>">invite</a></li>
+			<li><a class="owl_nav_button" href="<?php print site_url('owl/members'); ?>">list</a></li>
+			<li><a class="owl_nav_button" href="<?php print site_url('owl/members/requests'); ?>">requests</a></li>
+			<li><a class="owl_nav_button" href="<?php print site_url('owl/members/invite'); ?>">invite</a></li>
 			<li style="list-style-type: none">&nbsp;</li>
 		</ul>
 		<li style="list-style-type: none">catagories</li>
 		<ul>
-			<li class="owl_nav_button"><a href="<?php print site_url('owl/catagories'); ?>">list</a></li>
-			<li class="owl_nav_button"><a href="<?php print site_url('owl/catagories/organize'); ?>">organize</a></li>
-			<li class="owl_nav_button"><a href="<?php print site_url('owl/catagories/create'); ?>">create</a></li>
+			<li><a class="owl_nav_button" href="<?php print site_url('owl/catagories'); ?>">list</a></li>
+			<li><a class="owl_nav_button" href="<?php print site_url('owl/catagories/organize'); ?>">organize</a></li>
+			<li><a class="owl_nav_button" href="<?php print site_url('owl/catagories/create'); ?>">create</a></li>
 			<li style="list-style-type: none">&nbsp;</li>
 		</ul>
 		<li style="list-style-type: none">uploads</li>
 		<ul>
-			<a href="<?php print site_url('owl/uploads'); ?>"><li class="owl_nav_button">browse</li></a>
-			<a href="<?php print site_url('owl/uploads/upload'); ?>"><li class="owl_nav_button">upload</li></a>
+			<li><a class="owl_nav_button" href="<?php print site_url('owl/uploads'); ?>">browse</a></li>
+			<li><a class="owl_nav_button" href="<?php print site_url('owl/uploads/upload'); ?>">upload</a></li>
 			<li style="list-style-type: none">&nbsp;</li>
 		</ul>
 	</ul>
