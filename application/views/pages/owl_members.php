0efaf6b4c616ebb63a1756ffe02dfda11efcc124
Alan Wynn <wynn_45@hotmail.com>
2012-05-06 13:14:27 +0100

    style the owl pages

--- application/views/pages/owl_members.php
+++ application/views/pages/owl_members.php
@@ -6,57 +6,62 @@
 		</center>
 	</h1>
 
-	<div id="body">
-        <table cellspacing="0" cellpadding="4" border="1" width="100%">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Username</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Registration Date</th>
-                    <th>Admin</th>
-                    <th>Uploader</th>
-                </tr>
-            </thead>
-            <tbody>
+    <div id="body">
+        <div id="owl_nav">
+            <?php $this->load->view('pages/owl_nav'); ?>
+        </div>
+    	<div id="owl_body">
+            <table cellspacing="0" cellpadding="4" border="1" width="100%">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Registration Date</th>
+                        <th>Admin</th>
+                        <th>Uploader</th>
+                    </tr>
+                </thead>
+                <tbody>
 
 <?php
     if($members) :
     foreach($members->result() as $row) :
 ?>
-                <tr id="r-<?php print $row->id; ?>">
-                    <td><?php print $row->id; ?></td>
-                    <td><?php print $row->user_name; ?></td>
-                    <td><?php print $row->user_first_name; ?></td>
-                    <td><?php print $row->user_last_name; ?></td>
-                    <td><?php print date("H:i:s d/m/Y", $row->user_registration_date); ?></td>
-                    <td>
-                        <center>
-                        <?php if($this->session->userdata('admin')) : ?>
-                            <span style="color:#63b52e !important;" class="icon_font">.</span>
-                        <?php else : ?>
-                            <span style="color:#FF0000 !important;" class="icon_font">'</span>
-                        <?php endif; ?>
-                        </center>
-                    </td>
-                    <td>
-                        <center>
-                        <?php if($this->session->userdata('uploader')) : ?>
-                            <span style="color:#63b52e !important;" class="icon_font">.</span>
-                        <?php else : ?>
-                            <span style="color:#FF0000 !important;" class="icon_font">'</span>
-                        <?php endif; ?>
-                        </center>
-                    </td>
-                </tr>
+                    <tr id="r-<?php print $row->id; ?>">
+                        <td><?php print $row->id; ?></td>
+                        <td><?php print $row->user_name; ?></td>
+                        <td><?php print $row->user_first_name; ?></td>
+                        <td><?php print $row->user_last_name; ?></td>
+                        <td><?php print date("H:i:s d/m/Y", $row->user_registration_date); ?></td>
+                        <td>
+                            <center>
+                            <?php if($this->session->userdata('admin')) : ?>
+                                <span style="color:#63b52e !important;" class="icon_font">.</span>
+                            <?php else : ?>
+                                <span style="color:#FF0000 !important;" class="icon_font">'</span>
+                            <?php endif; ?>
+                            </center>
+                        </td>
+                        <td>
+                            <center>
+                            <?php if($this->session->userdata('uploader')) : ?>
+                                <span style="color:#63b52e !important;" class="icon_font">.</span>
+                            <?php else : ?>
+                                <span style="color:#FF0000 !important;" class="icon_font">'</span>
+                            <?php endif; ?>
+                            </center>
+                        </td>
+                    </tr>
 <?php
     endforeach;
     endif;
 ?>
 
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
 	</div>
 
 <?php $this->load->view('template/footer'); ?>
