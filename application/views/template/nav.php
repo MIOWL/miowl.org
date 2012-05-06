294cbeec1aaf677b6a0e38b78f1d7bff3e46063d
Alan Wynn <wynn_45@hotmail.com>
2012-05-06 10:51:11 +0100

    nav update

--- application/views/template/nav.php
+++ application/views/template/nav.php
@@ -1,21 +1,18 @@
+	<div id="welcome_nav">
+	<?php if ($this->session->userdata('authed')) : ?>
+			Welcome Back <?php print $this->session->userdata('name'); ?> (<a href='<?php print site_url('user/logout'); ?>' title='Logout'>logout</a>)
+	<?php else : ?>
+			Welcome Guest (<a href='<?php print site_url('user/login'); ?>' title='Login'>login</a> | <a href='<?php print site_url('user/register'); ?>' title='Register a new account'>register</a>)
+	<?php endif; ?>
+	</div>
 	<div id="nav">
-		<?php
-
+	<?php
 		if ($this->session->userdata('authed'))
 		{
 			# Logged in NAV
-			print "<a href='". site_url('user/logout') . "' title='Logout'>logout</a>";							# logout
-			print " | ";																						# spacer
 			print "<a href='". site_url('user/owl') . "' title='View your Owl'>my owl</a>";						# my owl
 			print " | ";																						# spacer
 			print "<a href='". site_url('upload') . "' title='Upload a new file'>upload</a>";					# upload
-		}
-		else
-		{
-			# Default NAV
-			print "<a href='". site_url('user/login') . "' title='Login'>login</a>"; 							# login
-			print " | ";																						# spacer
-			print "<a href='". site_url('user/register') . "' title='Register a new account'>register</a>";		# register
 		}
 
 		# These are non specific nav options
@@ -25,6 +22,5 @@
 		print "<a href='". site_url('search') . "' title='Search for a file'>search</a>";						# search
 		print " | ";																							# spacer
 		print "<a href='". site_url('about') . "' title='About the site'>about</a>";							# about
-
-		?>
+	?>
 	</div>
