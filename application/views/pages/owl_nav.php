3f927637dc695e6115cd202a576f9c2139dd38b2
Alan Wynn <wynn_45@hotmail.com>
2012-05-06 21:15:38 +0100

    owl nav updates

--- application/views/pages/owl_nav.php
+++ application/views/pages/owl_nav.php
@@ -3,7 +3,12 @@
 		<li>
 			members
 			<ul>
-				<li><a class="owl_nav_button" href="<?php print site_url('owl/members'); ?>">list</a></li>
+				<li>
+					<a class="owl_nav_button" href="<?php print site_url('owl/members'); ?>">list</a>
+					<li><a class="owl_nav_button" href="<?php print site_url('owl/members/admin'); ?>">admins</a></li>
+					<li><a class="owl_nav_button" href="<?php print site_url('owl/members/editor'); ?>">editors</a></li>
+					<li><a class="owl_nav_button" href="<?php print site_url('owl/members/user'); ?>">users</a></li>
+				</li>
 				<li><a class="owl_nav_button" href="<?php print site_url('owl/members/requests'); ?>">requests</a></li>
 				<li><a class="owl_nav_button" href="<?php print site_url('owl/members/invite'); ?>">invite</a></li>
 				<li style="list-style-type: none">&nbsp;</li>
