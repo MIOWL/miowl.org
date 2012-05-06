a4be67cae1039d9380920c1752753c7f78070997
Alan Wynn <wynn_45@hotmail.com>
2012-05-06 17:39:36 +0100

    side nav updates

--- application/views/pages/owl_nav.php
+++ application/views/pages/owl_nav.php
@@ -4,16 +4,19 @@
 			<li><a href="<?php print site_url('owl/members'); ?>">list</a></li>
 			<li><a href="<?php print site_url('owl/requests'); ?>">requests</a></li>
 			<li><a href="<?php print site_url('owl/invite'); ?>">invite</a></li>
+			<li>&nbsp;</li>
 		</ul>
 		<li>catagories</li>
 		<ul>
 			<li><a href="<?php print site_url('owl/catagories'); ?>">list</a></li>
 			<li><a href="<?php print site_url('owl/catagories/organize'); ?>">organize</a></li>
 			<li><a href="<?php print site_url('owl/catagories/create'); ?>">create</a></li>
+			<li>&nbsp;</li>
 		</ul>
 		<li>uploads</li>
 		<ul>
 			<li><a href="<?php print site_url('owl/uploads'); ?>">browse</a></li>
 			<li><a href="<?php print site_url('owl/uploads/upload'); ?>">upload</a></li>
+			<li>&nbsp;</li>
 		</ul>
 	</ul>
