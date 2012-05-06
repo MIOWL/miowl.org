8ce96356abbd45548437068022db74f26e7d84b5
Alan Wynn <wynn_45@hotmail.com>
2012-05-06 11:08:46 +0100

    nav update

--- application/views/template/nav.php
+++ application/views/template/nav.php
@@ -1,11 +1,10 @@
-	<div id="welcome_nav">
+	<div id="nav">
 	<?php if ($this->session->userdata('authed')) : ?>
 			Welcome Back <?php print $this->session->userdata('name'); ?> (<a href='<?php print site_url('user/logout'); ?>' title='Logout'>logout</a>)
 	<?php else : ?>
 			Welcome Guest (<a href='<?php print site_url('user/login'); ?>' title='Login'>login</a> | <a href='<?php print site_url('user/register'); ?>' title='Register a new account'>register</a>)
 	<?php endif; ?>
-	</div>
-	<div id="nav">
+	<br />
 	<?php
 		if ($this->session->userdata('authed'))
 		{
@@ -13,10 +12,10 @@
 			print "<a href='". site_url('user/owl') . "' title='View your Owl'>my owl</a>";						# my owl
 			print " | ";																						# spacer
 			print "<a href='". site_url('upload') . "' title='Upload a new file'>upload</a>";					# upload
+			print " | ";																						# spacer
 		}
 
 		# These are non specific nav options
-		print " | ";																							# spacer
 		print "<a href='". site_url('browse') . "' title='Browse all the files'>browse</a>";					# browse
 		print " | ";																							# spacer
 		print "<a href='". site_url('search') . "' title='Search for a file'>search</a>";						# search
