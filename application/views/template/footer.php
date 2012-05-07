c986faca0859cbcced7b67cd1e4b63db069c1e92
Alan Wynn <wynn_45@hotmail.com>
2012-05-07 20:06:29 +0100

    update key

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -22,7 +22,7 @@
 <?php if(isset($google_maps) && $google_maps) : ?>
 <!-- Google Maps -->
 <script type="text/javascript"
-  <?php $api_key = 'AIzaSyDgqrFeyh5GFQ08jcQ0PSDxhyeN2ciKzB0'; ?>
+  <?php $api_key = 'AIzaSyBCmt3NIalQwGdAz7fZMPqsatacXFZQ9To'; ?>
   src="https://maps.googleapis.com/maps/api/js?key=<?php print $api_key; ?>&sensor=false">
 </script>
 <script type="text/javascript">
