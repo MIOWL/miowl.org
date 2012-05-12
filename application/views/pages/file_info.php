fd7a4114c7044881acf29e82e6f668d1fec8318d
Alan Wynn <wynn_45@hotmail.com>
2012-05-12 20:49:28 +0100

    add info page view

---
+++ application/views/pages/file_info.php
@@ -0,0 +1,37 @@
+<?php $this->load->view('template/header'); ?>
+
+	<h1>
+		<center>
+			<?php print $page_title; ?>
+		</center>
+	</h1>
+
+	<div id="body">
+
+        <!--
+        Details Page :
+                        id
+                        upload_time
+                        upload_user
+                        owl
+                        file_name
+                        upload_catagory
+                        file_type
+                        file_size
+                        description
+        -->
+
+        <fieldset class="inlineLabels">
+
+            <div class="ctrlHolder">
+                <label for="id">File</label>
+                <span name="id" id="id" size="35" class="textInput medium" value="<?php print $details->row()->id; ?>">
+                    <?php print $details->row()->id; ?>
+                </span>
+            </div>
+
+        </fieldset>
+
+    </div>
+
+<?php $this->load->view('template/footer'); ?>
