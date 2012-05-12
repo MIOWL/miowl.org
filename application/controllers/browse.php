4a39956eda66c30428854060590a4eb55913017b
Alan Wynn <wynn_45@hotmail.com>
2012-05-12 20:07:06 +0100

    add file info link

--- application/controllers/browse.php
+++ application/controllers/browse.php
@@ -92,7 +92,7 @@ class Browse extends CI_Controller {
                     $this->miowl_model->get_category($row->upload_catagory)->row()->name,
                     $row->file_type,
                     $this->owl_model->get_owl_by_id($row->owl)->row()->owl_name,
-                    '<center><a href="' . site_url('download/' . $row->id) . '" title="Downlaod this file!" target="_BLANK" class="icon_font">F</a></center>'
+                    '<center><a href="' . site_url('download/' . $row->id) . '" title="Downlaod this file!" target="_BLANK" class="icon_font">F</a></center>',
                     '<center><a href="' . site_url('info/' . $row->id) . '" title="More info for this file!" class="icon_font">,</a></center>'
                 );
             }
