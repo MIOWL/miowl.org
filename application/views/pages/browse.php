7faf83eb5bbafe8094508da5d66bc992ea6c935d
Alan Wynn <wynn_45@hotmail.com>
2012-05-19 11:16:29 +0100

    try pagenation

--- application/views/pages/browse.php
+++ application/views/pages/browse.php
@@ -11,8 +11,6 @@
 		<?php print $table; ?>
 
 		<?php
-			$this->load->library('pagination');
-
 			$config['base_url'] = $base_url;
 			$config['total_rows'] = $total_rows;
 			$config['per_page'] = $per_page;
