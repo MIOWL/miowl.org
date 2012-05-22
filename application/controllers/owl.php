69aac32798ead7013c02f1703afbd430c5629169
Alan Wynn <wynn_45@hotmail.com>
2012-05-22 20:00:37 +0100

    categories update

--- application/controllers/owl.php
+++ application/controllers/owl.php
@@ -883,43 +883,4 @@ class Owl extends CI_Controller {
     //------------------------------------------------------------------
 
 
-    /**
-     * private gen_categories()
-     */
-    private function gen_categories()
-    {
-        $cat_array = NULL;
-
-        // get our root categories
-        if (($roots = $this->cat_model->get_roots()))
-        {
-            foreach ($roots->result() as $root)
-            {
-                $cat_array[$root->name] = array();
-
-                // child this roots children
-                if (($kids = $this->cat_model->get_children($root->id)))
-                {
-                    foreach ($kids->result() as $child)
-                    {
-                        $cat_array[$root->name][$child->name] = array();
-
-                        // get our childs children
-                        if (($kids_kids = $this->cat_model->get_children($child->id)))
-                        {
-                            foreach ($kids_kids->result() as $childs_childs)
-                            {
-                                $cat_array[$root->name][$child->name][$childs_childs->name] = array();
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return $cat_array;
-    }
-    //------------------------------------------------------------------
-
-
 }
