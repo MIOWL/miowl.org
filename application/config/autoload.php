2250586d0cb7691c27af6971b12f153080a3e2a5
Alan Wynn <wynn_45@hotmail.com>
2012-05-22 19:45:43 +0100

    categories update

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array('user_model', 'owl_model', 'miowl_model', 'upload_model');
+$autoload['model'] = array('user_model', 'owl_model', 'miowl_model', 'upload_model', 'cat_model');
 
 
 /* End of file autoload.php */
