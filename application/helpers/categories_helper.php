757cbd3585a36c7760275cd292063d8999fda3c8
Alan Wynn <wynn_45@hotmail.com>
2012-05-22 19:19:32 +0100

    categories update

--- application/helpers/categories_helper.php
+++ application/helpers/categories_helper.php
@@ -3,25 +3,26 @@
 if (!function_exists('gen_categories'))
 {
     function gen_categories()
-    {   
+    {
+        $CI =& get_instance();
         $cat_array = NULL;
 
         // get our root categories
-        if (($roots = $this->cat_model->get_roots()))
+        if (($roots = $CI->cat_model->get_roots()))
         {
             foreach ($roots->result() as $root)
             {
                 $cat_array[$root->name] = array();
 
                 // child this roots children
-                if (($kids = $this->cat_model->get_children($root->id)))
+                if (($kids = $CI->cat_model->get_children($root->id)))
                 {
                     foreach ($kids->result() as $child)
                     {
                         $cat_array[$root->name][$child->name] = array();
 
                         // get our childs children
-                        if (($kids_kids = $this->cat_model->get_children($child->id)))
+                        if (($kids_kids = $CI->cat_model->get_children($child->id)))
                         {
                             foreach ($kids_kids->result() as $childs_childs)
                             {
