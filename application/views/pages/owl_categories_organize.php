9b37cb7318541730582cc9573d6b602d531674da
Alan Wynn <wynn_45@hotmail.com>
2012-05-22 22:51:23 +0100

    categories update

--- application/views/pages/owl_categories_organize.php
+++ application/views/pages/owl_categories_organize.php
@@ -12,41 +12,11 @@
         </div>
         <div id="owl_body" class="column right threequarter">
 
-            <div id="default_categories">
-                <h2>Default Categories</h2>
-                <span class="h2_note">NOTE: these cannot be modified in any way...</span>
+            <div id="categories_list">
+                <h2>Categories</h2>
                 <?php
-                    $categories_list = array();
-                    $attributes = array('id' => 'default_categories_list');
-                    foreach ($categories->result() as $row) {
-                        if ($row->owl_id === "0") {
-                            if($row->parent_id === "0") {
-                                // Root Category
-                                $categories_list[] = $row->name;
-                            }
-                        }
-                    }
-                    print ul($categories_list, $attributes);
-                ?>
-            </div>
-
-            <br><hr><br>
-
-            <div id="custom_categories">
-                <h2>Custom Categories</h2>
-                <?php
-                    $categories_list = array();
-                    $attributes = array('id' => 'custom_categories_list');
-                    foreach ($categories->result() as $row) {
-                        if ($row->owl_id != "0") {
-                            if($row->parent_id === "0") {
-                                // Root Category
-                                $categories_list[] = $row->name;
-                            }
-                        }
-                    }
-                    if (!empty($categories_list))
-                        print ul($categories_list, $attributes);
+                    if (!empty(gen_categories()))
+                        print ul(gen_categories(), array('id' => 'categories'));
                     else
                         print "You have no custom categories created...";
                 ?>
