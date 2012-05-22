c6c22e9d0a4a1a115be708639f6c2e5a384af562
Alan Wynn <wynn_45@hotmail.com>
2012-05-22 19:33:19 +0100

    categories update

--- application/views/pages/owl_categories.php
+++ application/views/pages/owl_categories.php
@@ -17,7 +17,36 @@
 $categories_list = array();
 $attributes = array('id' => 'categories');
 
-print ul(gen_categories(), $attributes);
+$cat_array = NULL;
+
+// get our root categories
+if (($roots = $this->cat_model->get_roots()))
+{
+    foreach ($roots->result() as $root)
+    {
+        $cat_array[$root->name] = array();
+
+        // child this roots children
+        if (($kids = $this->cat_model->get_children($root->id)))
+        {
+            foreach ($kids->result() as $child)
+            {
+                $cat_array[$root->name][$child->name] = array();
+
+                // get our childs children
+                if (($kids_kids = $this->cat_model->get_children($child->id)))
+                {
+                    foreach ($kids_kids->result() as $childs_childs)
+                    {
+                        $cat_array[$root->name][$child->name][$childs_childs->name] = array();
+                    }
+                }
+            }
+        }
+    }
+}
+
+print ul($cat_array, $attributes);
 
 ?>
 
