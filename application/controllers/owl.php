1cd1c934a8a690e79ac1e94fc1a416f32e77f2c8
Alan Wynn <wynn_45@hotmail.com>
2012-05-22 19:44:16 +0100

    categories update

--- application/controllers/owl.php
+++ application/controllers/owl.php
@@ -550,14 +550,7 @@ class Owl extends CI_Controller {
         // page data array
         $page_data                  = array();
         $page_data['page_title']    = "All Owl File Categories";
-
-
-        $page_data['root_categories'] = $this->miowl_model->get_owl_categories($this->session->userdata('owl'), TRUE, 21);
-        //print $this->db->last_query();
-        //die();
-
-
-        //$page_data['categories']    = $this->miowl_model->get_owl_categories($this->session->userdata('owl'));
+        $page_data['categories']    = $this->gen_categories();
 
         // load the approp. page view
         $this->load->view('pages/owl_categories', $page_data);
@@ -886,6 +879,45 @@ class Owl extends CI_Controller {
         curl_close($ch);
 
         return $data;
+    }
+    //------------------------------------------------------------------
+
+
+    /**
+     * private gen_categories()
+     */
+    private function gen_categories()
+    {
+        $cat_array = NULL;
+
+        // get our root categories
+        if (($roots = $this->cat_model->get_roots()))
+        {
+            foreach ($roots->result() as $root)
+            {
+                $cat_array[$root->name] = array();
+
+                // child this roots children
+                if (($kids = $this->cat_model->get_children($root->id)))
+                {
+                    foreach ($kids->result() as $child)
+                    {
+                        $cat_array[$root->name][$child->name] = array();
+
+                        // get our childs children
+                        if (($kids_kids = $this->cat_model->get_children($child->id)))
+                        {
+                            foreach ($kids_kids->result() as $childs_childs)
+                            {
+                                $cat_array[$root->name][$child->name][$childs_childs->name] = array();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return $cat_array;
     }
     //------------------------------------------------------------------
 
--- application/views/pages/owl_categories.php
+++ application/views/pages/owl_categories.php
@@ -11,45 +11,7 @@
             <?php $this->load->view('pages/owl_nav'); ?>
         </div>
         <div id="owl_body" class="column right threequarter">
-
-<?php
-
-$categories_list = array();
-$attributes = array('id' => 'categories');
-
-$cat_array = NULL;
-
-// get our root categories
-if (($roots = $this->cat_model->get_roots()))
-{
-    foreach ($roots->result() as $root)
-    {
-        $cat_array[$root->name] = array();
-
-        // child this roots children
-        if (($kids = $this->cat_model->get_children($root->id)))
-        {
-            foreach ($kids->result() as $child)
-            {
-                $cat_array[$root->name][$child->name] = array();
-
-                // get our childs children
-                if (($kids_kids = $this->cat_model->get_children($child->id)))
-                {
-                    foreach ($kids_kids->result() as $childs_childs)
-                    {
-                        $cat_array[$root->name][$child->name][$childs_childs->name] = array();
-                    }
-                }
-            }
-        }
-    }
-}
-
-print ul($cat_array, $attributes);
-
-?>
-
+            <?php print ul($categories, array('id' => 'categories')); ?>
         </div>
         <div class="clear">&nbsp;</div>
     </div>
