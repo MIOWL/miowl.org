6aa3e07b1ac075d81c829f948ac5fcca20f4eb35
Alan Wynn <wynn_45@hotmail.com>
2012-05-23 16:58:26 +0100

    browse updates

--- application/views/pages/file_info.php
+++ application/views/pages/file_info.php
@@ -57,7 +57,7 @@
 
                 <div class="ctrlHolder">
                     <label for="upload_category">category</label>
-                    <span name="upload_category" id="upload_category" size="35" class="textInput medium"><?php print $this->miowl_model->get_category($info->row()->upload_category)->row()->name; ?></span>
+                    <span name="upload_category" id="upload_category" size="35" class="textInput medium"><?php print $this->cat_model->get_category($info->row()->upload_category)->row()->name; ?></span>
                 </div>
 
                 <div class="ctrlHolder">
