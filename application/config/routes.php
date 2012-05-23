0d940e4dae3ed40d71d617df4668f35dc23bafa3
Alan Wynn <wynn_45@hotmail.com>
2012-05-23 17:11:25 +0100

    info page updates

--- application/config/routes.php
+++ application/config/routes.php
@@ -48,7 +48,7 @@ $route['about'] 							= 'welcome/about';
 $route['download/(:num)'] 					= 'browse/download/$1';
 $route['owl/uploads/upload'] 				= 'upload/index';
 $route['browse/(:num)'] 					= 'browse/index/$1';
-$route['deleted/info/(:num)'] 				= 'browse/info/$1/TRUE'
+$route['deleted/info/(:num)'] 				= 'browse/info/$1/TRUE';
 
 
 /* End of file routes.php */
