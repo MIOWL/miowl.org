4a341d1a155273ff569f8de0906d1dd1aa00cf34
Alan Wynn <wynn_45@hotmail.com>
2012-05-29 20:30:19 +0100

    search bar updates

--- application/views/search/__search_bar.php
+++ application/views/search/__search_bar.php
@@ -1,4 +1,4 @@
-<form class="searchform">
-	<input type="text" onblur="if (this.value == '') {this.value = 'Search...';}" onfocus="if (this.value == 'Search...') {this.value = '';}" value="Search..." class="searchfield">
-	<input type="button" value="Go" class="searchbutton">
+<form id="search_bar" style="position: relative; display: inline-block; bottom: 45px; width: 300px; left: 345px; margin-right: 0pt; padding-left: 24px;">
+	<input type="text" class="textInput small" value="Search..." onfocus="if (this.value == 'Search...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Search...';}">
+	<input type="button" class="button" value="GO">
 </form>
