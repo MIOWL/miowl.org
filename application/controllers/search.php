87b17b8ff563ad533c438996038a8e7e33ae4766
Alan Wynn <wynn_45@hotmail.com>
2012-05-30 13:31:28 +0100

    search form

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -64,11 +64,10 @@ class Search extends CI_Controller {
      *
      * Search Results Page
      */
-    public function results()
+    public function results($offset = 0)
     {
         // fetch the data from the get param
         $keyword    = $this->input->get('keyword');
-        $offset     = is_null($this->input->get('per_page')) ? ($this->input->get('per_page') * $this->per_page_limit) : 0;
 
         // setup our page data
         $page_data = array();
@@ -79,7 +78,7 @@ class Search extends CI_Controller {
         // setup pagination lib
         // $config['base_url']         = base_url('search/' . $offset . '/results.php');
         $config['base_url']         = base_url('search/' . $offset . '/results.php?keyword=' . $keyword);
-        $config['uri_segment']      = 2;
+        // $config['uri_segment']      = 2;
         $config['total_rows']       = (($rows = $this->search_model->search_all($keyword, FALSE, FALSE))) ? $rows->num_rows() : 0;
         $config['per_page']         = $this->per_page_limit;
         $config['anchor_class']     = 'class="button" ';
