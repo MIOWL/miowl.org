ae41657daf177cebdc4092df30f7bea1baf4b271
Alan Wynn <wynn_45@hotmail.com>
2012-05-30 14:59:44 +0100

    search form

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -66,8 +66,8 @@ class Search extends CI_Controller {
      */
     public function results($offset = 0)
     {
-        print '<pre>' . print_r($this->uri->uri_to_assoc() , TRUE) . '</pre>';
-        
+        print '<pre>' . $this->uri->uri_to_assoc() . '</pre>';
+
         // fetch the data from the get param
         $keyword    = $this->input->get('keyword');
 
