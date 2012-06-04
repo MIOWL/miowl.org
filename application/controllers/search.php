898932d1c17621d1b68d2154fd71e1937d0b4e81
Alan Wynn <wynn_45@hotmail.com>
2012-06-04 19:03:41 +0100

    search updates

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -71,7 +71,6 @@ class Search extends CI_Controller {
         }
         else
         {
-            print '<pre>' . print_r($this->db->last_query(), TRUE) . '</pre>';
             // load our view
             $this->load->view('search/form', $page_data);
         }
@@ -99,7 +98,7 @@ class Search extends CI_Controller {
         $page_data['keyword'] = $search['keyword'];
         $page_data['query'] = $this->gen_results($search['keyword'], $offset, $this->per_page_limit);
 
-        print '<pre>' . print_r($this->db->last_query(), TRUE) . '</pre>';
+        // print '<pre>' . print_r($this->db->last_query(), TRUE) . '</pre>';
 
         // setup pagination lib
         $config['base_url']         = base_url('search/results');
