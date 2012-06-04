7804545bf45d7c81ae22ad1bb0aef2119821754f
Alan Wynn <wynn_45@hotmail.com>
2012-06-04 13:34:08 +0100

    owl/owls nav update

--- application/views/pages/owl_nav.php
+++ application/views/pages/owl_nav.php
@@ -1,10 +1,11 @@
+<?php $uri = $this->uri->segment(1); ?>
 	<ul>
 
 		<li>
 			MiOwl
 			<ul>
 				<li><a class="owl_nav_button" href="<?php print site_url(); ?>">details</a></li>
-<?php if ($this->session->userdata('admin')) : ?>
+<?php if ($uri === 'owl' && $this->session->userdata('admin')) : ?>
 				<li><a class="owl_nav_button" href="<?php print site_url('owl/edit_details'); ?>">edit details</a></li>
 <?php endif; ?>
 				<li style="list-style-type: none">&nbsp;</li>
@@ -15,17 +16,18 @@
 			members
 			<ul>
 				<li>
-					<a class="owl_nav_button" href="<?php print site_url('owl/members'); ?>">list</a>
+					<a class="owl_nav_button" href="<?php print site_url($uri . '/members'); ?>">list</a>
 					<ul>
-						<li><a class="owl_nav_button" href="<?php print site_url('owl/members/admin'); ?>">admins</a></li>
-						<li><a class="owl_nav_button" href="<?php print site_url('owl/members/editor'); ?>">editors</a></li>
-						<li><a class="owl_nav_button" href="<?php print site_url('owl/members/user'); ?>">users</a></li>
+						<li><a class="owl_nav_button" href="<?php print site_url($uri . '/members/admin'); ?>">admins</a></li>
+						<li><a class="owl_nav_button" href="<?php print site_url($uri . '/members/editor'); ?>">editors</a></li>
+						<li><a class="owl_nav_button" href="<?php print site_url($uri . '/members/user'); ?>">users</a></li>
 					</ul>
 				</li>
-<?php if ($this->session->userdata('admin')) : ?>
+<?php if($uri === 'owl') : if ($this->session->userdata('admin')) : ?>
 				<li><a class="owl_nav_button" href="<?php print site_url('owl/members/requests'); ?>">requests</a></li>
 <?php endif; ?>
 				<li><a class="owl_nav_button" href="<?php print site_url('owl/members/invite'); ?>">invite</a></li>
+<?php endif; ?>
 				<li style="list-style-type: none">&nbsp;</li>
 			</ul>
 		</li>
@@ -33,8 +35,8 @@
 		<li>
 			categories
 			<ul>
-				<li><a class="owl_nav_button" href="<?php print site_url('owl/categories'); ?>">list</a></li>
-<?php if ($this->session->userdata('admin')) : ?>
+				<li><a class="owl_nav_button" href="<?php print site_url($uri . '/categories'); ?>">list</a></li>
+<?php if ($uri === 'owl' && $this->session->userdata('admin')) : ?>
 				<li><a class="owl_nav_button" href="<?php print site_url('owl/categories/organize'); ?>">organize</a></li>
 				<li><a class="owl_nav_button" href="<?php print site_url('owl/categories/create'); ?>">create</a></li>
 <?php endif; ?>
@@ -45,12 +47,12 @@
 		<li>
 			uploads
 			<ul>
-				<li><a class="owl_nav_button" href="<?php print site_url('owl/uploads'); ?>">browse</a></li>
-<?php if ($this->session->userdata('editor')) : ?>
+				<li><a class="owl_nav_button" href="<?php print site_url($uri . '/uploads'); ?>">browse</a></li>
+<?php if($uri === 'owl') : if ($this->session->userdata('editor')) : ?>
 				<li><a class="owl_nav_button" href="<?php print site_url('owl/uploads/upload'); ?>">upload</a></li>
 <?php endif; if ($this->session->userdata('admin')) : ?>
 				<li><a class="owl_nav_button" href="<?php print site_url('owl/uploads/bin'); ?>">recycle bin</a></li>
-<?php endif; ?>
+<?php endif; endif; ?>
 				<li style="list-style-type: none">&nbsp;</li>
 			</ul>
 		</li>
