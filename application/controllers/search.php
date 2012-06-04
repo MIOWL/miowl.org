c741003e122da17dceae81f1410bd2d87d26f503
Alan Wynn <wynn_45@hotmail.com>
2012-06-04 11:41:25 +0100

    search updates

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -136,7 +136,7 @@ class Search extends CI_Controller {
         $where      = array();
         $having     = array();
 
-        $search = $POST_data ? $this->input->post(NULL, TRUE) : $this->session->userdata('search');
+        $search = $this->input->post(NULL, TRUE); # : $this->session->userdata('search');
 
         print '<script type="text/javascript">alert("';
         print $POST_data ? '$this->input->post(NULL, TRUE)' : '$this->session->userdata(\'search\')';
