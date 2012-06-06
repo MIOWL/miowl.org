8fd8f3b739ba92eac870082effd8642d3e387b41
Alan Wynn <wynn_45@hotmail.com>
2012-06-06 14:19:27 +0100

    owl choice page

--- application/views/pages/owl_choice.php
+++ application/views/pages/owl_choice.php
@@ -7,35 +7,27 @@
 	</h1>
 
 	<div id="body">
-        <div id="owl_nav" class="column left quarter">
-            <?php $this->load->view('pages/owl_nav'); ?>
-        </div>
-        <div id="owl_body" class="column right threequarter">
+        <form action="" class="uniForm" method="post">
 
-            <form action="" class="uniForm" method="post">
+            <?php $this->load->view('messages/message_inline'); ?>
 
-                <?php $this->load->view('messages/message_inline'); ?>
-
-                <fieldset class="inlineLabels">
-                    <div class="ctrlHolder">
-                        <label for="owl">Organization</label>
-                        <select name="owl" id="owl" class="textInput medium" autocompelete="OFF" />
-                            <?php foreach ($this->owl_model->get_all_owls()->result() as $owl) : ?>
-                                <option value="<?php print $owl->id; ?>" <?php print set_select('owl', $owl->id); ?>><?php print $owl->owl_name; ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                        <p class="formHint">Choose the organization you wish to browse, aka owl.</p>
-                    </div>
-                </fieldset>
-
-                <div class="buttonHolder">
-                    <button class="button" type="submit">Choose</button>
+            <fieldset class="inlineLabels">
+                <div class="ctrlHolder">
+                    <label for="owl">Organization</label>
+                    <select name="owl" id="owl" class="textInput medium" autocompelete="OFF" />
+                        <?php foreach ($this->owl_model->get_all_owls()->result() as $owl) : ?>
+                            <option value="<?php print $owl->id; ?>" <?php print set_select('owl', $owl->id); ?>><?php print $owl->owl_name; ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                    <p class="formHint">Choose the organization you wish to browse, aka owl.</p>
                 </div>
+            </fieldset>
 
-            </form>
+            <div class="buttonHolder">
+                <button class="button" type="submit">Choose</button>
+            </div>
 
-        </div>
-        <div class="clear">&nbsp;</div>
+        </form>
     </div>
 
 <?php $this->load->view('template/footer'); ?>
