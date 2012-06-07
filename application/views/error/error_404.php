960e3a6fc82bb988252fe01139679861ccfb0e97
Alan Wynn <wynn_45@hotmail.com>
2012-06-07 16:02:46 +0100

    undo remove 404 route

--- application/views/error/error_404.php
+++ application/views/error/error_404.php
@@ -1,20 +1,30 @@
-<?php $this->load->view('template/header'); ?>
+<?php $meh =& get_instance(); ?>
+<?php $meh->load->view('template/header'); ?>
 
 	<h1>
 		<center>
-			404 Page Not Found!
+			<?php echo $heading; ?>
 		</center>
 	</h1>
 
 	<div id="body">
 		<center>
-			<h3>
-				The page you requested was not found.
-			</h3>
+
+			<div class="notification error" style="width: 400px ! important; text-align: left ! important;">
+			        <p><strong>Error:</strong> </p><p><?php echo $message; ?></p>
+			<p></p>
+			</div>
+
 			<p>
 				Maybe you should just <a href="<?php print site_url(); ?>">pop back home?</a>
 			</p>
+
 		</center>
 	</div>
 
-<?php $this->load->view('template/footer'); ?>
+  <div class="clear">&nbsp;</div>
+	<p class="footer">&nbsp;</p>
+</div>
+
+</body>
+</html>
