af87cb5bb28a7f9a9c3d339259165d8a223f2ca2
Alan Wynn <wynn_45@hotmail.com>
2012-06-12 11:58:37 +0100

    search page updates

--- application/views/search/form.php
+++ application/views/search/form.php
@@ -116,7 +116,7 @@
         // checkAll/uncheckAll functions
         function checkAll(field)
         {
-            $(field).each(function (i)
+            field.each(function (i)
             {
                 $(this).checked = true ;
             });
@@ -124,9 +124,9 @@
 
         function uncheckAll(field)
         {
-            $(field).each(function (i)
+            field.each(function (i)
             {
-                $(this).checked = false ;
+                this.checked = false ;
             });
         }
     </script>
