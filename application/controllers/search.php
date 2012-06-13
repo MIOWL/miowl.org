477a830d65bbc185103ffb43f38489caae179a99
Alan Wynn <wynn_45@hotmail.com>
2012-06-13 17:44:57 +0100

    search page updates

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -168,7 +168,7 @@ class Search extends CI_Controller {
         $output = $return_data === FALSE ? array() : array_unique($return_data, SORT_STRING);
 
         // set our JSON header
-        #@header('Content-type: application/json');
+        @header('Content-type: application/json');
 
         // print out our output in JSON
         print json_encode($output);
