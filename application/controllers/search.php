c3fac8bfb8781405aaa6b4a88dc3df2af927c25b
Alan Wynn <wynn_45@hotmail.com>
2012-06-13 10:09:10 +0100

    search page updates

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -116,7 +116,7 @@ class Search extends CI_Controller {
         $page_data['keyword'] = $search['keyword'];
         $page_data['query'] = $this->gen_results($offset, $this->per_page_limit);
 
-        // print '<pre>' . print_r($this->db->last_query(), TRUE) . '</pre>';
+        print '<pre>' . print_r($this->db->last_query(), TRUE) . '</pre>';
 
         // setup pagination lib
         $config['base_url']         = base_url('search/results');
