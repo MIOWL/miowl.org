58d6f397875790ee9626ddc552e5ced43b19007d
Alan Wynn <wynn_45@hotmail.com>
2012-06-13 16:43:16 +0100

    search page updates

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -159,7 +159,7 @@ class Search extends CI_Controller {
         elseif ($type == 'province') {
                 if(($data = $this->owl_model->get_owl_by_province($value))) {
                     $return_data = array();
-                    foreach ($data-results() as $row) {
+                    foreach ($data->results() as $row) {
                         $return_data[$row->id] = $row->owl_name;
                     }
                 }
