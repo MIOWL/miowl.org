a5870ea88db8944b1714dae05dd0e335447b1dd8
Alan Wynn <wynn_45@hotmail.com>
2012-06-14 11:32:04 +0100

    search jQuery updates

--- application/views/search/form.php
+++ application/views/search/form.php
@@ -60,7 +60,7 @@
                     <div id="owl-selection" class="ctrlHolder" >
                         <label for="owl">Search within specific owls?</label>
                         <div id="owl_list" style="display: inline-block;" >
-                            <?php foreach ($this->owl_model->get_all_owls()->result() as $row) : ?>
+                            <?php/* foreach ($this->owl_model->get_all_owls()->result() as $row) : ?>
                                 <input
                                     type="checkbox"
                                     name="owl[]"
@@ -73,7 +73,7 @@
                                     ?>
                                 />&nbsp;&nbsp;&nbsp;&nbsp;<?php print $row->owl_name; ?>
                                 <br />
-                            <?php endforeach; ?>
+                            <?php endforeach; */?>
                             <span class="save button"   onclick="checkAll('.owl_list')"   > Check All </span>
                             <span class="delete button" onclick="uncheckAll('.owl_list')" > Uncheck All </span>
                         </div>
