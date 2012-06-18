5553e8ee36a41e52aecaf4f65b7e1f94e82f0220
Alan Wynn <wynn_45@hotmail.com>
2012-06-18 12:37:20 +0100

    JS updates

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -12,6 +12,9 @@
     <!-- CSS -->
     <link rel="stylesheet" href="<?php print site_url('/style.css'); ?>" type="text/css" media="screen" charset="utf-8" />
 
+    <!-- jQuery UI CSS -->
+    <link rel="stylesheet" href="<?php print site_url('/css/jquery-ui.css'); ?>" type="text/css" media="screen" charset="utf-8" />
+
 	<!-- jQuery -->
 	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js"></script>
 	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.js"></script>
