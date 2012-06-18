d6da6a5fba77522bd3f8b765006e6a8cc8a58470
Alan Wynn <wynn_45@hotmail.com>
2012-06-18 20:11:17 +0100

    updates

--- application/views/search/__table.php
+++ application/views/search/__table.php
@@ -25,10 +25,10 @@
 					<?php print $row->file_type; ?>
 				</td>
 				<td>
-					<a href="<?php print site_url('browse/owl/' . $row->up_id); ?>" title="Downlaod this file!" target="_BLANK" class="icon_font">F</a>
+					<a href="<?php print site_url('browse/download/' . $row->up_id); ?>" title="Downlaod this file!" target="_BLANK" class="icon_font">F</a>
 				</td>
 				<td>
-					<a href="<?php print site_url('browse/owl/' . $row->up_id); ?>" title="More info for this file!" class="icon_font">,</a>
+					<a href="<?php print site_url('browse/info/' . $row->up_id); ?>" title="More info for this file!" class="icon_font">,</a>
 				</td>
 			</tr>
 		<?php endforeach; ?>
