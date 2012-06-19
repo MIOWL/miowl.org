1d498c302f827b16e627c062261b25f98f58f4e6
Alan Wynn <wynn_45@hotmail.com>
2012-06-19 12:34:34 +0100

    user promote/demote updates

--- application/views/pages/owl_members.php
+++ application/views/pages/owl_members.php
@@ -99,7 +99,7 @@
                     title: camesString(action) + ' the user?',
                     autoOpen: true,
                     resizable: false,
-                    height:151,
+                    // height:151,
                     modal: true,
                     buttons: {
                         "Confirm": function() {
