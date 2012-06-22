8718071f63335c8b31a738296582ffbd96bf1c06
Alan Wynn <wynn_45@hotmail.com>
2012-06-22 13:32:32 +0100

    upload updates

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -107,65 +107,64 @@ class Upload extends CI_Controller {
             // else
             // {
             }
-            return;
-                print "validated\n";
-                $upload_data = $this->upload->data();
+            //     print "validated\n";
+            //     $upload_data = $this->upload->data();
 
-                $file_name = $this->input->post('filename') == ""
-                                ? $upload_data['client_name']
-                                : $this->input->post('filename');
+            //     $file_name = $this->input->post('filename') == ""
+            //                     ? $upload_data['client_name']
+            //                     : $this->input->post('filename');
 
-                $upload_user        = $this->session->userdata('user_id');
-                $owl                = $this->session->userdata('owl');
-                $full_path          = $upload_data['full_path'];
-                $upload_category    = $this->input->post('category');
-                $upload_license     = $this->input->post('license');
-                $file_type          = $upload_data['file_type'];
-                $client_name        = $upload_data['client_name'];
-                $file_size          = $upload_data['file_size'];
-                $file_ext           = $upload_data['file_ext'];
-                $description        = str_replace(array("\r\n","\r","\n"), '\n', trim($this->input->post('description')));
+            //     $upload_user        = $this->session->userdata('user_id');
+            //     $owl                = $this->session->userdata('owl');
+            //     $full_path          = $upload_data['full_path'];
+            //     $upload_category    = $this->input->post('category');
+            //     $upload_license     = $this->input->post('license');
+            //     $file_type          = $upload_data['file_type'];
+            //     $client_name        = $upload_data['client_name'];
+            //     $file_size          = $upload_data['file_size'];
+            //     $file_ext           = $upload_data['file_ext'];
+            //     $description        = str_replace(array("\r\n","\r","\n"), '\n', trim($this->input->post('description')));
 
-                $revDate            = $this->input->post('revDate') == ""
-                                        ? NULL
-                                        : strtotime($this->input->post('revDate'));
+            //     $revDate            = $this->input->post('revDate') == ""
+            //                             ? NULL
+            //                             : strtotime($this->input->post('revDate'));
 
-                if (!$this->upload_model->add_upload(
-                            $upload_user,
-                            $owl,
-                            $file_name,
-                            $full_path,
-                            $upload_category,
-                            $upload_license,
-                            $file_type,
-                            $client_name,
-                            $file_size,
-                            $file_ext,
-                            $description,
-                            $revDate
-                          ))
-                    {
-                        $page_data['page_title'] = 'Upload Failure';
-                        $page_data['error'] = TRUE;
-                        $page_data['msg'] = "Something went wrong with the database...";
-                        $this->load->view('pages/upload_form', $page_data);
-                        return;
-                    }
-                else {
-                    print " -- good db insert\n";
-                }
+            //     if (!$this->upload_model->add_upload(
+            //                 $upload_user,
+            //                 $owl,
+            //                 $file_name,
+            //                 $full_path,
+            //                 $upload_category,
+            //                 $upload_license,
+            //                 $file_type,
+            //                 $client_name,
+            //                 $file_size,
+            //                 $file_ext,
+            //                 $description,
+            //                 $revDate
+            //               ))
+            //         {
+            //             $page_data['page_title'] = 'Upload Failure';
+            //             $page_data['error'] = TRUE;
+            //             $page_data['msg'] = "Something went wrong with the database...";
+            //             $this->load->view('pages/upload_form', $page_data);
+            //             return;
+            //         }
+            //     else {
+            //         print " -- good db insert\n";
+            //     }
 
-                $page_data['page_title'] = 'Upload Success';
-                $page_data['upload_data'] = $upload_data;
+            //     $page_data['page_title'] = 'Upload Success';
+            //     $page_data['upload_data'] = $upload_data;
 
-                // Success Message
-                $page_data['success']     = TRUE;
-                $page_data['msg']         = "Upload Successful, upload another?";
+            //     // Success Message
+            //     $page_data['success']     = TRUE;
+            //     $page_data['msg']         = "Upload Successful, upload another?";
 
-                $this->load->view('pages/upload_success', $page_data);
-                #$this->load->view('pages/upload_form', $page_data);
-                return;
-            }
+            //     $this->load->view('pages/upload_success', $page_data);
+            //     #$this->load->view('pages/upload_form', $page_data);
+            //     return;
+            // }
         }
     }
     //------------------------------------------------------------------
