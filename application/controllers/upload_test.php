214b59818580fcd82638bde73f83453f311ff7fc
Alan Wynn <wynn_45@hotmail.com>
2012-06-22 14:07:45 +0100

    upload debug

--- application/controllers/upload_test.php
+++ application/controllers/upload_test.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Upload extends CI_Controller {
+class Upload_test extends CI_Controller {
 
     //=================================================================================
     // :private vars
