a5849347d45acc9380105073dc62eb0dd07a25a3
Alan Wynn <wynn_45@hotmail.com>
2012-06-23 11:46:17 +0100

    added the cron outlined class

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -32,12 +32,25 @@ class Cron extends CI_Controller {
 
     /**
      * public takeOutTheTrash()
-     * this function is used to do the nightly cleanup via the cron task.
+     * this function is used to do the nightly cleanup via the cron task
      * it delete's the files that were deleted 30 days ago
      */
     public function takeOutTheTrash()
     {
-        print "Sorry, this function is not yet coded";
+        print "Sorry, this function is not yet coded".PHP_EOL;
+    }
+    //------------------------------------------------------------------
+
+
+    /**
+     * public inactiveMembers()
+     * this function is used to do the nightly members check via the cron task
+     * it will check to see if a member registered over 30 days ago and re-send the activation email
+     * it will also remove members and owls that were not activated and are over 90 days old
+     */
+    public function inactiveMembers()
+    {
+        print "Sorry, this function is not yet coded".PHP_EOL;
     }
     //------------------------------------------------------------------
 
