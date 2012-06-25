8b21ff50a3c946cf7b60e18b1eaf6c5e77b09833
Alan Wynn <wynn_45@hotmail.com>
2012-06-25 12:48:47 +0100

    cat organize

--- application/views/pages/owl_categories_organize.php
+++ application/views/pages/owl_categories_organize.php
@@ -12,13 +12,6 @@
         </div>
         <div id="owl_body" class="column right threequarter">
 
-            <!--
-            <div id="categories_list">
-                <h2>Categories</h2>
-                <?php print ul(gen_categories(), array('id' => 'categories')) ?>
-            </div>
-            -->
-
             <table>
                 <thead>
                     <tr>
@@ -33,7 +26,6 @@
 <?php
     if($categories) :
     foreach($categories->result() as $row) :
-        $edit = $remove = 'todo';
 ?>
                     <tr id="r-<?php print $row->id; ?>">
                         <td><?php print cat_breadcrumb_ul($row->id); ?><?php /*print $row->name;*/ ?></td>
@@ -48,7 +40,7 @@
                             <a style="color:#63b52e !important;" href="<?php print $row->id; ?>"><img src="/images/icons/edit.gif" title="edit this category" alt="edit" width="16px" height="16px" /></a>
                         </td>
                         <td>
-                            <a style="color:#FF0000 !important;" class="icon_font" href="<?php print $row->id; ?>">'</a>
+                            <a style="color:#FF0000 !important;" href="<?php print $row->id; ?>"><img src="/images/icons/trash.png" title="edit this category" alt="edit" width="20px" height="20px" /></a>
                         </td>
                     </tr>
 <?php
