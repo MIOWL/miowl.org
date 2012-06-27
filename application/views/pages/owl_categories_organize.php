172a674e9d30c16d1ce4130288b3efdcecadde0a
Alan Wynn <wynn_45@hotmail.com>
2012-06-27 15:14:18 +0100

    cat organize

--- application/views/pages/owl_categories_organize.php
+++ application/views/pages/owl_categories_organize.php
@@ -129,7 +129,7 @@
                     var select_list = response;
 
                     // create and load the dialog form
-                    $('<div></div>').html('<p class="validateTips">All form fields are required.</p><form><fieldset><label for="name" class="right">Category Name</label><input type="text" name="name" id="dialog_name" class="text ui-widget-content ui-corner-all left" value="' + cat_name + '" /><br /><br /><label for="subcat" class="right">Sub Category</label><select name="subcat" id="dialog_subcat" class="select ui-widget-content ui-corner-all left">' + select_list + 'M/select></fieldset></form>').dialog({
+                    $('<div></div>').html('<p class="validateTips">All form fields are required.</p><form><fieldset><label for="name" class="left">Category Name</label><input type="text" name="name" id="dialog_name" class="text ui-widget-content ui-corner-all right" value="' + cat_name + '" /><br /><br /><label for="subcat" class="left">Sub Category</label><select name="subcat" id="dialog_subcat" class="select ui-widget-content ui-corner-all right">' + select_list + 'M/select></fieldset></form>').dialog({
                         title: 'Edit the Category',
                         autoOpen: true,
                         resizable: false,
