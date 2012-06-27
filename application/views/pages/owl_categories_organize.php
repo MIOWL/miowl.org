317ecc1264011613ffc8bec8ff8c607965ce31d9
Alan Wynn <wynn_45@hotmail.com>
2012-06-27 12:06:27 +0100

    cat organize

--- application/views/pages/owl_categories_organize.php
+++ application/views/pages/owl_categories_organize.php
@@ -158,7 +158,7 @@
 
                 $.get('/owl/categories/select_list/' + cat_pid), function(response) {
                     var select_list = response;
-                }, "html");
+                } );
 
                 // create and load the dialog form
                 $('<div></div>').html('<p class="validateTips">All form fields are required.</p><form><fieldset><label for="name">Category Name</label><input type="text" name="name" id="name" class="text ui-widget-content ui-corner-all" value="' + cat_name + '" /><label for="subcat">Sub Category</label><select name="subcat" id="subcat" class="select ui-widget-content ui-corner-all">' + select_list + 'M/select></fieldset></form>').dialog({
@@ -191,7 +191,7 @@
                                         $.get('/owl/categories/select_list/' + cat_id), function(data) {
                                             // var breadcrumb = response;
                                             $('td:first', $('#r-' + cat_id)).html(data);
-                                        }, "html");
+                                        } );
 
                                         // update the href to reflect this change
                                         element.attr('href', cat_id + ':' + response.subcat + ':' + response.name);
