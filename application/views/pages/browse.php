d5a1503f57fc90b1387d4a971aa11035efbcfa0b
Alan Wynn <wynn_45@hotmail.com>
2012-07-10 16:06:26 +0100

    updates

--- application/views/pages/browse.php
+++ application/views/pages/browse.php
@@ -2,7 +2,8 @@
 
 	<h1>
 		<center>
-			<?php if (isset($browse_by_owl) && $browse_by_owl) || (isset($browse_by_cat) && $browse_by_cat) : ?><a href="javascript:history.go(-1)" title="Back to the default browse page"><< back</a> | <?php endif; ?><?php print $page_title; ?>
+			<?php
+            if (isset($browse_by_owl) && $browse_by_owl) || (isset($browse_by_cat) && $browse_by_cat) : ?><a href="javascript:history.go(-1)" title="Back to the default browse page"><< back</a> | <?php endif; ?><?php print $page_title; ?>
 		</center>
 	</h1>
 
