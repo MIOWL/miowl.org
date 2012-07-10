95f90bc67ccd480a052253c253fe2ff49721f874
Alan Wynn <wynn_45@hotmail.com>
2012-07-10 13:58:42 +0100

    updates

--- application/views/pages/owl_licenses.php
+++ application/views/pages/owl_licenses.php
@@ -77,7 +77,7 @@
                 e.preventDefault();
 
                 // get the data from the form
-                var id   = $(this).closest('tr').attr('id').val(),
+                var id   = $(this).closest('tr').attr('id').replace("r-", ""),
                     name = $('td:first', $('#r-' + id)),
                     desc = $(name).next(),
                     url  = $(desc).next();
