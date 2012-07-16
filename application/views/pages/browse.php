a236bbd76ca43c1a6769735df2974b13058d9a27
Alan Wynn <wynn_45@hotmail.com>
2012-07-16 09:46:39 +0100

    browse updates

--- application/views/pages/browse.php
+++ application/views/pages/browse.php
@@ -31,7 +31,7 @@
         foreach($data->result() as $row) :
             $lic = $this->miowl_model->get_license($row->upload_license);
 ?>
-                    <tr id="r-<?php print $row->id; ?>">
+                    <tr id="r-<?php print $row->id; ?>" class="<?php if( ( !is_null( $row->revision_date ) ) && ( time() >= $row->revision_date ) ) print 'review'; ?>">
                         <td><a href="<?php print site_url('browse/owl/' . $row->owl); ?>"><?php print $this->owl_model->get_owl_by_id($row->owl)->row()->owl_name; ?></a></td>
                         <td><?php print cat_breadcrumb_ul($row->upload_category); ?></td>
                         <td><?php print $row->file_name; ?></td>
