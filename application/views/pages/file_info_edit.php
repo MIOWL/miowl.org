7e9c1f1ae07bc6a38651b7001f6fc48464321e31
Alan Wynn <wynn_45@hotmail.com>
2012-07-17 11:45:56 +0100

    file info editor updates

--- application/views/pages/file_info_edit.php
+++ application/views/pages/file_info_edit.php
@@ -49,7 +49,7 @@
 
                 <div class="ctrlHolder">
                     <label for="description">description</label>
-                    <textarea name="description" id="description" size="35" class="textInput large" rows="5" cols="50"><?php print trim($upload_info->row()->description)); ?></textarea>
+                    <textarea name="description" id="description" size="35" class="textInput large" rows="5" cols="50"><?php print trim($upload_info->row()->description); ?></textarea>
                     <!--span name="description" id="description" class="textInput medium"><?php print str_replace(array("\n", '\n'), "<br>", $upload_info->row()->description); ?></span-->
                 </div>
 
