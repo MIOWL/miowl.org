1c55ab6e5f2d21dfc7fd49531c6118f7a6a3879a
Alan Wynn <wynn_45@hotmail.com>
2012-07-26 11:35:22 +0100

    resend validation

--- application/controllers/owl.php
+++ application/controllers/owl.php
@@ -242,7 +242,7 @@ class Owl extends CI_Controller {
                 $owl_info = $this->owl_model->get_owl_by_id($this->input->post('owl'));
                 $this->owl_model->choose_owl($this->session->userdata('user_id'), $this->input->post('owl'));
                 $this->owlmail->send_chosen($name, $owl_info->row()->owl_name, $this->session->userdata('email'));
-                $this->owlmail->inform_admin($name, $owl_info->row()->owl_email);
+                die( $this->owlmail->inform_admin($name, $owl_info->row()->owl_email) );
 
                 $page_data['success']   = TRUE;
                 $page_data['msg']       = "Successfully chosen you're owl. Please check your email to finish the registration process.";
