4760411f7ef2a86d3d77f1703f2a7349f38e2851
Alan Wynn <wynn_45@hotmail.com>
2012-07-31 22:42:32 +0100

    remove miowl model

--- application/views/template/owl_change.php
+++ application/views/template/owl_change.php
@@ -1,18 +1,20 @@
-<?php
-    $user_owls = $this->user_model->get_owls();
-    if ($user_owls->num_rows() > 1) :
-?>
-    <div id="owl_choice_area" class="right">
-        Active Owl
-        <br />
-        <select id="current_owl_chosen" autocompelete="OFF" />
-            <?php foreach ($user_owls->result() as $owl_row) : ?>
-                <option value="<?php print $owl_row->owl; ?>" <?php print ($this->session->userdata('owl') == $owl_row->owl) ? 'selected' : NULL; ?>>
-                    <?php print $this->owl_model->get_owl_by_id($owl_row->owl)->row()->owl_name; ?>
-                </option>
-            <?php endforeach; ?>
-        </select>
-        <br />
-        <a href="change_owl" class="button right">Change</a></li>
-    </div>
-<?php endif; ?>
+<div style="position: relative; float: right; top: 150px; right: -55px;">
+	<div style="left: auto; right: auto; position: absolute;" id="owl_choice_area" class="right">
+		<?php
+		    $user_owls = $this->user_model->get_owls();
+		    if ($user_owls->num_rows() > 1) :
+		?>
+		    <div id="owl_choice_area" class="right">
+		        <span>Active Owl</span>
+		        <br />
+		        <select id="current_owl_chosen" autocompelete="OFF" />
+		            <?php foreach ($user_owls->result() as $owl_row) : ?>
+		                <option value="<?php print $owl_row->owl; ?>" <?php print ($this->session->userdata('owl') == $owl_row->owl) ? 'selected' : NULL; ?>><?php print $this->owl_model->get_owl_by_id($owl_row->owl)->row()->owl_name; ?></option>
+		            <?php endforeach; ?>
+		        </select>
+		        <br />
+		        <a href="change_owl" class="button right">Change</a></li>
+		    </div>
+		<?php endif; ?>
+	</div>
+</div>
