a25539bad2b2f3698cc42f0117b447858ae9342a
Alan Wynn <wynn_45@hotmail.com>
2012-07-31 21:43:28 +0100

    remove miowl model

--- application/views/pages/owl_nav.php
+++ application/views/pages/owl_nav.php
@@ -48,10 +48,11 @@
 			<li style="list-style-type: none">Active Owl</li>
 			<li style="list-style-type: none">
 	            <select id="current_owl_chosen" autocompelete="OFF" />
-	            <?php foreach ($user_owls->results() as $owl_row) : ?>
-                    <option value="<?php print $owl_row->owl; ?>" <?php print ($this->session->userdata('owl') == $owl_row->owl) ? 'selected' : NULL; ?>>
-                    	<?php print $this->owl_model->get_owl_by_id($owl_row->owl)->row()->owl_name; ?>
-                    </option>
+		            <?php foreach ($user_owls->results() as $owl_row) : ?>
+	                    <option value="<?php print $owl_row->owl; ?>" <?php print ($this->session->userdata('owl') == $owl_row->owl) ? 'selected' : NULL; ?>>
+	                    	<?php print $this->owl_model->get_owl_by_id($owl_row->owl)->row()->owl_name; ?>
+	                    </option>
+	                <?php endforeach; ?>
 	            </select>
 			</li>
 			<li style="list-style-type: none"><a href="change_owl" class="button">Change</a></li>
