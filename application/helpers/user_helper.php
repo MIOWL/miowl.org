d4581d106830d3ee7dfadeb3be83dc892d9f0cc3
Alan Wynn <wynn_45@hotmail.com>
2012-07-31 18:43:47 +0100

    remove miowl model

--- application/helpers/user_helper.php
+++ application/helpers/user_helper.php
@@ -33,11 +33,13 @@
  * ------------------------------------------------------------------------------
  */
 
+$CI =& get_instance();
+
 if (!function_exists('is_verified'))
 {
     function is_verified()
     {
-        return $this->owl_model->is_x('verified');
+        return $CI->owl_model->is_x('verified');
     }
 }
 
@@ -46,7 +48,7 @@ if (!function_exists('is_admin'))
 {
     function is_admin()
     {
-        return $this->owl_model->is_x('admin');
+        return $CI->owl_model->is_x('admin');
     }
 }
 
@@ -55,7 +57,7 @@ if (!function_exists('is_editor'))
 {
     function is_editor()
     {
-        return $this->owl_model->is_x('editor');
+        return $CI->owl_model->is_x('editor');
     }
 }
 
@@ -64,7 +66,7 @@ if (!function_exists('is_owner'))
 {
     function is_owner()
     {
-        return $this->owl_model->is_x('owner');
+        return $CI->owl_model->is_x('owner');
     }
 }
 
