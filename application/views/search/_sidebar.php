74b3f4cef4d0af533c5f9aaedbe808e910759590
Alan Wynn <wynn_45@hotmail.com>
2012-08-03 23:07:57 +0100

    search sidebar

--- application/views/search/_sidebar.php
+++ application/views/search/_sidebar.php
@@ -1,10 +1,14 @@
+<?php
+	$search = $this->session->userdata('search');
+	$post 	= $this->session->userdata('post');
+?>
 	Search Filters
 	<ul>
 
 		<li>
 			keyword
 			<ul>
-				<li><?php print ($this->session->userdata('search'))['keyword']; ?></li>
+				<li><?php print $search['keyword']; ?></li>
 				<li style="list-style-type: none">&nbsp;</li>
 			</ul>
 		</li>
@@ -12,7 +16,7 @@
 		<li>
 			owl type
 			<ul>
-				<li><?php print $this->session->userdata('post')['type']; ?></li>
+				<li><?php print $post['type']; ?></li>
 				<li style="list-style-type: none">&nbsp;</li>
 			</ul>
 		</li>
@@ -20,7 +24,7 @@
 		<li>
 			owl province(s)
 			<ul>
-				<?php foreach ($this->session->userdata('search')['having']['owl_province'] as $province) : ?>
+				<?php foreach ($search['having']['owl_province'] as $province) : ?>
 					<li><?php print $province; ?></li>
 				<?php endforeach; ?>
 				<li style="list-style-type: none">&nbsp;</li>
@@ -30,7 +34,7 @@
 		<li>
 			owl(s)
 			<ul>
-				<?php foreach ($this->session->userdata('search')['having']['owl_id'] as $owl_id) : ?>
+				<?php foreach ($search['having']['owl_id'] as $owl_id) : ?>
 					<li><?php print $owl_id; ?></li>
 				<?php endforeach; ?>
 				<li style="list-style-type: none">&nbsp;</li>
