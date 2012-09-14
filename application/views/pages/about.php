db3d3c390cc23210dd348f3ab0c94ccb64b65dbf
Alan Wynn <wynn_45@hotmail.com>
2012-09-14 14:18:09 +0100

    Added inline nav for tabs/content

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -15,17 +15,23 @@
                 <li><a href="#disclaimer">Disclaimer &amp; Privacy</a></li>
             </ul>
             <div id="who_we_are">
-                <h2>Genesis</h2>
+                <ul class="out-links">
+                    <li><a href="#Genesis">Genesis</a></li>
+                    <li><a href="#Principles">Principles</a></li>
+                    <li><a href="#Goals">Goals</a></li>
+                </ul>
+                <div class="clear"></div>
+                <h2 id="Genesis">Genesis</h2>
                 <p>
                     The development of Medical Interprofessional Open source Web-based Library (MI OWL) occurred when a group of physicians in the rural, remote community of Marathon, in Northwestern Ontario, became a Family Health Team in 2005.  Becoming an interprofessional team was an impetus to create a variety of new programs for our patient population.  We were challenged in this endeavor by the lack of available information on such clinical innovations and the inability to easily share information with other clinical teams.  With an Innovations grant from the Ontario Ministry of Health and Long Term Care one of the Marathon Family Health Team physicians, Eliseo Orrantia, embarked on creating MI OWL with the help of Joseph Newing (IT) and Margaret Cousins (epidemiologist).  We employed the services of Alan Wynn, of friendlydev.com as the primary programmer.
                 </p>
                 <br />
-                <h2>Principles</h2>
+                <h2 id="Principles">Principles</h2>
                 <p>
                     MI OWL is rooted in several principles which include being: a free service, advertisement free, open source and widely accessible.  MI OWL is a noncommercial site.  Its creators do not wish to make any financial profit from its use, nor have others do so.  Being open source allows for the program to be improved upon by other interested parties.  It can also be adapted, with minor modifications, to provide a similar service in other countries.  Beyond the health care application, the basic programming of MI OWL could be used to make a library of libraries for any myriad of topics.   The primary intent of MI OWL is to support health care providers in their care provision within our Canadian health care system.  As our system is publically funded, we believe that the body of knowledge that it creates should be freely accessible to all.  Improved sharing among health care providers can enhance the system’s function and evolution.  As well, transparency with the public makes us more directly accountable.
                 </p>
                 <br />
-                <h2>Goals</h2>
+                <h2 id="Goals">Goals</h2>
                 <p>
                     MI OWL has been created to provide Canadian primary care clinics and small community hospitals with a tool to create their own web-based document libraries.  These libraries allow the organizing, searching and sharing of the information that health care facilities rely on to provide patient care.   Such information can be policies, program descriptions, forms, medical directives and patient resources for example.  Individual libraries can be organized as desired depending on need and preference.
                 </p>
@@ -34,10 +40,15 @@
                 </p>
             </div>
             <div id="howto">
-                <h2>Browsing libraries</h2>
+                <ul class="out-links">
+                    <li><a href="#BrowseLibraries">Browsing libraries</a></li>
+                    <li><a href="#SearchContent">Searching content</a></li>
+                </ul>
+                <div class="clear"></div>
+                <h2 id="BrowseLibraries">Browsing libraries</h2>
                 <p>You can look through individual libraries by clicking on “OWLs” on the top right of MI OWL pages.  This will bring you to the OWL Choice page.  From there you can select the desired library from the drop down list by clicking on “choose”, in the lower right of that screen.  You will be taken to the particular library’s OWL Details page.   Clicking “list” under the heading of “categories”, on the left hand side of that page, will bring up the table of contents for that particular OWL.  Clicking on a particular category within that list will bring up all the document files stored under that category.  More information on a particular file can then be discovered by clicking on the tag icon to the right of the file name.  Another way to look through the files in a particular OWL is to click on “browse” under the “uploads” heading on the left side of an OWL’s Details page.  This will bring up a list of all the files within that OWL.  More information on a particular file can be discovered by clicking on its tag icon to the right of the file name.  Files can be downloaded, or printed by clicking on the appropriate icon to the right of the file name.</p>
                 <br/>
-                <h2>Searching content</h2>
+                <h2 id="SearchContent">Searching content</h2>
                 <p>Content within the MI OWL site and all its libraries can be searched by clicking on “site search” at the top left of the MI OWL’s pages.  This will bring up the general “Search form” page where you will be prompted to enter 3 different search limits and then the topic to be searched.  Clicking on “search” on the lower right of the Search Form page will then run the inquiry.</p>
             </div>
             <div id="create_owl">
@@ -84,6 +95,22 @@
         #tabs h2 {
             border-bottom: 1px solid #000;
             padding-bottom: 5px;
+        }
+
+        .out-links {
+            margin: 0;
+            padding: 0;
+            float: right;
+        }
+
+        .out-links li {
+            display: inline-block;
+        }
+
+        .out-links li:not(:first-child):before {
+            color: #888888;
+            content: "/";
+            padding: 0 1em;
         }
     </style>
 
